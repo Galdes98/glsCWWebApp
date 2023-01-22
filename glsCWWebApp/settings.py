18b83985453dd3d58a1ef62b044c3904d25a1fa1
danielmaidana34 <daniel.hahn34@gmail.com>
2023-01-22 17:04:58 -0300

    se agrega awsconnection

--- glsCWWebApp/settings.py
+++ glsCWWebApp/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -75,8 +81,12 @@ WSGI_APPLICATION = 'glsCWWebApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME':'postgres',
+        'USER': 'galdes',
+        'PASSWORD':'xYQusR8pxviJFnL',
+        'HOST':'glscoweight.cydfhxahljat.us-east-1.rds.amazonaws.com',
+        'PORT':'5432',
     }
 }
 
@@ -121,3 +131,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/home'
+LOGIN_REDIRECT_URL = '/login'
