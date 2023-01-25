490f38b44f494fc4742c3a8d34c8e78d7c4b759f
danielmaidana34 <daniel.hahn34@gmail.com>
2023-01-25 10:32:17 -0300

    se agrego seleccion para personal o jefe

--- main/forms.py
+++ main/forms.py
@@ -3,12 +3,18 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from.models import Post
 
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+MOD = [
+    ('','Personal'),
+    ('','Jefe de personal'),
+]
 
+class RegisterForm(UserCreationForm):
+    
+    email = forms.EmailField(required=True)
+    mod = forms.ChoiceField(choices=MOD)
     class Meta:
         model = User
-        fields = ["username","email","password1", "password2"]
+        fields = ["username","email","mod","password1", "password2"]
 
 class PostForm(forms.ModelForm):
     class Meta:
