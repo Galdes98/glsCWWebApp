9f27e543abe0b9238894475c11a0fa4311ccedb3
Galdes98 <bruno.vallejos98@gmail.com>
2023-10-02 23:00:03 -0300

    sacado cosas del dockerfile no validos y nuevo host en settings.py

--- glsCWWebApp/settings.py
+++ glsCWWebApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!w9#)wlzffllvrc2mw3@-_0r2@b+-p978h$t9gvz$u5jx(a)$+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.19']
 
 
 # Application definition
